dbe11264341e8c6686800c3e03beb2c2c80624fc
BlakeWu <wuronghua@52tt.com>
2019-06-11 11:49:40 +0800

    注释掉packageDescrption

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
 // swift-tools-version:4.2
 
-import PackageDescription
+//import PackageDescription
 
-let package = Package(
-    name: "PhoneNumberKit",
-    products: [
-        .library(name: "PhoneNumberKit", targets: ["PhoneNumberKit"])
-    ],
-    targets: [
-        .target(name: "PhoneNumberKit", path: "PhoneNumberKit", exclude: ["UI"]),
-        .testTarget(name: "PhoneNumberKitTests", dependencies: ["PhoneNumberKit"], path: "PhoneNumberKitTests")
-    ]
-)
+//let package = Package(
+//    name: "PhoneNumberKit",
+//    products: [
+//        .library(name: "PhoneNumberKit", targets: ["PhoneNumberKit"])
+//    ],
+//    targets: [
+//        .target(name: "PhoneNumberKit", path: "PhoneNumberKit", exclude: ["UI"]),
+//        .testTarget(name: "PhoneNumberKitTests", dependencies: ["PhoneNumberKit"], path: "PhoneNumberKitTests")
+//    ]
+//)
